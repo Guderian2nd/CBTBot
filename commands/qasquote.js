a87a4c4e33c76eeccba66f151fb8e3ab77c74aa7
Guderian2nd <gshin3@live.com>
2021-02-06 01:55:20 +0900

    qasquote edit sentence

--- commands/qasquote.js
+++ commands/qasquote.js
@@ -7,7 +7,11 @@ module.exports = {
     var coin = Math.random();
     if (coin < 0.5) {
       var noun = sentencer.make('{{ noun }}');
-      msg.channel.send(`\"I once dated a girl who liked ${noun}(s), so I hate ${noun}(s).\" - Qastiel`);
+      if (coin < 0.25) {
+        msg.channel.send(`\"I once dated a girl who liked ${noun}(s), so I hate ${noun}(s).\" - Qastiel`);
+      } else {
+        msg.channel.send(`\"I once dated a girl who liked ${noun}(s), so ${noun}(s) are dumb.\" - Qastiel`);
+      }
     } else {
       var an_adjective = sentencer.make('{{ an_adjective }}');
       var adjective = an_adjective.split(/ +/g);
