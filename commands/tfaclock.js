1dd734b13806fe0e44f96c42dbd502d1653931ac
Guderian2nd <gshin3@live.com>
2021-03-16 09:44:00 +0900

    fix tfaclock function

--- commands/tfaclock.js
+++ commands/tfaclock.js
@@ -6,14 +6,17 @@ module.exports = {
   execute(msg, args) {
     var date = new Date();
     var counter = date.getTime() - globals.tfacounter.getTime();
+    counter = date.getTime() - globals.tfacounter.getTime();
     counter = counter / (1000 * 3600 * 24);
     counter = Math.floor(counter);
 
     if (args[0] == 'reset') {
-      msg.channel.send('CBT complained about TFA again! Resetting the clock...');
+      msg.channel.send(`CBT complained about TFA again! Resetting the clock...The record this time was ${counter} days.`);
       globals.tfacounter = date;
+      msg.channel.send('0 days since the last time CBT complained about TFA.');
     }
-
-    msg.channel.send(`${counter} days since the last time CBT complained about TFA.`);
+    else {
+      msg.channel.send(`${counter} days since the last time CBT complained about TFA.`);
+    }
   },
 };
