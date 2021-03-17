2fb0f304e24d0904ed79e7b01561d3a1aeca96f4
Guderian2nd <gshin3@live.com>
2021-03-17 13:15:28 +0900

    cbt bot count by hours not days

--- commands/tfaclock.js
+++ commands/tfaclock.js
@@ -7,16 +7,19 @@ module.exports = {
     var date = new Date();
     var counter = date.getTime() - globals.tfacounter.getTime();
     counter = date.getTime() - globals.tfacounter.getTime();
-    counter = counter / (1000 * 3600 * 24);
+    counter = counter / (1000 * 3600);
     counter = Math.floor(counter);
 
+    var hours = counter % 24;
+    var days = Math.floor(counter / 24)
+
     if (args[0] == 'reset') {
-      msg.channel.send(`CBT complained about TFA again! Resetting the clock...The record this time was ${counter} days.`);
+      msg.channel.send(`CBT complained about TFA again! Resetting the clock...The record this time was ${days} day(s), ${hours} hour(s).`);
       globals.tfacounter = date;
-      msg.channel.send('0 days since the last time CBT complained about TFA.');
+      msg.channel.send('0 day(s), 0 hour(s) since the last time CBT complained about TFA.');
     }
     else {
-      msg.channel.send(`${counter} days since the last time CBT complained about TFA.`);
+      msg.channel.send(`${days} day(s), ${hours} hour(s) since the last time CBT complained about TFA.`);
     }
   },
 };
