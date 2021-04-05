dbfb78c6458b674f88adaa2f1a3f5938db5b46a2
Guderian2nd <gshin3@live.com>
2021-04-06 01:29:28 +0900

    reconfigure globals

---
+++ globals.js
@@ -0,0 +1,22 @@
+// global variables
+module.exports = {
+  ctflag : false,
+  ctdiscrim : '2163',
+  guddiscrim : '6580',
+  catodiscrim : '8057',
+  ctsheet : '<:CTSheet:779498877158031372>',
+  koreanworkinghours : '<:koreanworkhours:782245163623120917>',
+  catonoturn : '<:whencatodoesntposttheturn:695400172938854450>',
+  catoday : 0,
+  tfacounter : 0,
+  tfakeywords : [ 'GWK',
+    'Hoss',
+    'Wirehoss',
+    'Ashley',
+    'Floof',
+    ' TFA',
+    'TFA ',
+    'Atrium',
+    'HOWF',
+    'Simpli']
+}
