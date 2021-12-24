09e5fba42857e1de0c867920d3b7b325755600c4
Guderian2nd <gshin3@live.com>
2021-12-24 22:09:43 +0900

    fix setinveral not firing bug

--- commands/stevdraw.js
+++ commands/stevdraw.js
@@ -1,20 +1,31 @@
 const globals = require('../globals.js');
+function convertTZ(date, tzString) {
+  return new Date((typeof date === 'string' ? new Date(date) : date).toLocaleString('en-US', { timeZone: tzString }));
+}
 
 module.exports = {
   name: 'stevdraw',
   description: 'Tells stev to draw something at 8PM PST. Automatically called at start of day.',
   execute(client) {
-    if (typeof client == 'Client') {
-        client.setInterval( () =>
+    const tmp = client.user.discriminator;
+    if (tmp === '8715') 
+    {
+      console.log(`stev client is ready`);
+      if (typeof client.stevinterval == 'undefined')
+      {
+        console.log('no existing interval');
+        client.stevinterval = setInterval( () =>
         {
           const thedate = new Date();
           thedate = convertTZ(thedate, 'America/Los_Angeles');
           if ( thedate.getHours() === 20 && thedate.getMinutes() === 0)
           {
             globals.drawchannel.send(`${globals.stevuser}, go draw something u little bitch`);
+            clearInterval(client.stevinterval);
           }
           console.log(`stev time check : ${thedate}`);
         }, 60000) // check every minute
+      }
     }
   },
 };
